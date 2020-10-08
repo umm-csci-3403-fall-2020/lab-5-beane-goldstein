ee42a41f1edc0f3af5fdf0fad6dfa540a9497974
Robert-Beane <beane031@morris.umn.edu>
2020-10-08 10:58:26 -0500

    Create client branch with starter code

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,11 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+    public static final int portNumber = 6013;
+
+    public static void main(String[] args) throws IOException {
+
+    }
 }
