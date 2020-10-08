7e167132dd04707f9f70edddd902079a22a8955d
Ben <golds349@morris.umn.edu>
2020-10-08 12:17:34 -0500

    Add code to close socket
    If the program encounters an error and terminates early, or if future code exists to manually terminate the server, the socket is closed.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -6,8 +6,9 @@ public class EchoServer {
     public static final int portNumber = 6013;
 
     public static void main(String[] args) {
+        ServerSocket sock = null;
         try{
-            ServerSocket sock = new ServerSocket(portNumber);
+             sock = new ServerSocket(portNumber);
 
             while (true) {
                 Socket client = sock.accept();
@@ -31,6 +32,15 @@ public class EchoServer {
         } catch (IOException ioe) {
             System.out.println("We caught an unexpected exception");
             System.err.println(ioe);
+        } finally {
+            try {
+                if (sock != null) {
+                    sock.close();
+                }
+            } catch (IOException ioe) {
+                System.out.println("Unexpected error when trying to close the socket");
+                System.err.println(ioe);
+            }
         }
     }
 }
