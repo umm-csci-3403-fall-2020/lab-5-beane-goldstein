492562b6b107111319eb9f7f8614514b7ea9103b
Ben <golds349@morris.umn.edu>
2020-10-08 11:45:20 -0500

    Add starter code for server branch
    Add preliminary code for server branch based heavily on the dateServer example.  Tests have not yet been run.

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,36 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+    public static final int portNumber = 6013;
+
+    public static void main(String[] args) {
+        try{
+            ServerSocket sock = new ServerSocket(portNumber);
+
+            while (true) {
+                Socket client = sock.accept();
+                System.out.println("Got a request!");
+
+                ByteArrayInputStream byteInputStream = new ByteArrayInputStream(new byte[1]);
+                ByteArrayOutputStream byteOutputSteam = new ByteArrayOutputStream();
+
+                int inputByte;
+                while (true) {
+                    inputByte = byteInputStream.read();
+                    if (inputByte != -1){
+                        byteOutputSteam.write(inputByte);
+                    } else {
+                        break;
+                    }
+                }
+
+                client.close();
+            } 
+        } catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
 }
